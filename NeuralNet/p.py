925cbd4da234fbf7945cdee0eff65f61e584bbbe
anujbohra23 <anujbohra10@gmail.com>
2024-11-15 12:11:33 +0530

    dense layer object with forward pass

--- NeuralNet/p.py
+++ NeuralNet/p.py
@@ -1,16 +1,62 @@
-inputs = [1, 2, 3, 2.5]
+import numpy as np
+np.random.seed(0)
 
-# 3 neurons wiht 4 inputs 
-weights1 = [0.2,0.8,-0.5,1] #weight of neuron 1 
-weights2 = [0.5,-0.91,0.26,-0.5] #weight of neuron 2
-weights3 = [-0.26,-0.27,0.17,0.87] #weight of neuron 3
+X = [[1, 2, 3, 2.5],
+          [2,5,-1,2],
+          [-1.5,2.7,3.3,-0.8]]
 
-bias1 = 2  #bias of neuron 1
-bias2 = 3  #bias of neuron 2
-bias3 = 0.5 #bias of neuron 3
+# weights in the range of -1 and 1 (smaller the better) so that there isn't explosion of values in training
 
-output = [inputs[0] * weights1[0] + inputs[1] * weights1[1] + inputs[2] * weights1[2] + inputs[3] * weights1[3] + bias1,
-          inputs[0] * weights2[0] + inputs[1] * weights2[1] + inputs[2] * weights2[2] + inputs[3] * weights2[3] + bias2,
-          inputs[0] * weights3[0] + inputs[1] * weights3[1] + inputs[2] * weights3[2] + inputs[3] * weights3[3] + bias3]
-print(output)
-#prints output of each neuron
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+        
+layer1 = Layer_Dense(4,5)
+layer2 = Layer_Dense(5,2) # output of layer 1 is input to layer 2 that's why n_inputs for layer 2 has to be 5 since it was n_neurons for layer 1 and it required for dot product
+
+
+layer1.forward(X)
+print(layer1.output)
+layer2.forward(layer1.output)
+print(layer2.output)
+
+
+
+
+
+
+
+
+ 
+# weights = [[0.2,0.8,-0.5,1],
+#            [0.5,-0.91,0.26,-0.5],
+#            [-0.26,-0.27,0.17,0.87]]
+
+# biases1 = [2,3,0.5] 
+
+# weights2 = [[0.1,-0.14,0.5],
+#            [-0.5,0.12,-0.33],
+#            [-0.44,0.73,-0.13]]
+
+# biases2 = [-1,2,-0.5] 
+
+# layer1_outputs = np.dot(inputs, np.array(weights).T) + biases1
+# print(layer1_outputs)
+
+
+# layer2_outputs =np.dot(layer1_outputs, np.array(weights2).T) + biases2
+# print(layer2_outputs)
+
+
+
+
+
+
+
+
+
+
+
