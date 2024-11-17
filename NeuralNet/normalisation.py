4d4001cccde3a6183a621b5eb962fd6f9cbd664c
anujbohra23 <anujbohra10@gmail.com>
2024-11-17 12:37:41 +0530

    softmax layer after relu

--- NeuralNet/normalisation.py
+++ NeuralNet/normalisation.py
@@ -1,20 +1,22 @@
 import math
 import numpy as np
+import nnfs
+nnfs.init()
 
-layer_outputs = [4.3,3.3,4.4]
+layer_outputs = [[4.8,1.21,2.385],
+                 [8.9,-1.81,0.2],
+                 [1.41,1.051,0.026]]
 E = math.e
 
-exp_values = []
+exp_values = np.exp(layer_outputs)
 
-for i in layer_outputs:
-    exp_values.append(E**i)
-print(exp_values)
+# print(exp_values)
 
-norm_base = sum(exp_values)
-norm_values = []
+print(np.sum(layer_outputs,axis = 1, keepdims = True))
 
-for value in exp_values:
-    norm_values.append(value/norm_base)
+norm_values = exp_values/np.sum(exp_values,axis = 1, keepdims = True)
+
+
 print(norm_values)
-print(sum(norm_values))
+# print(sum(norm_values))
     
--- NeuralNet/p.py
+++ NeuralNet/p.py
@@ -5,27 +5,6 @@ from nnfs.datasets import spiral_data  # See for code: https://gist.github.com/S
 nnfs.init()
 
 
-
-
-X = [[1, 2, 3, 2.5],
-          [2,5,-1,2],
-          [-1.5,2.7,3.3,-0.8]]
-
-X, y = spiral_data(100, 3)   
-
-inputs= [0.2,-1,3.3,-2.7,1.1,2.2,-100]
-output = []
-
-for i in inputs:
-    if i>0:
-        output.append(max(0,i))
-    elif i<=0:
-        output.append(0)
-print(output)
-        
-
-#weights in the range of -1 and 1 (smaller the better) so that there isn't explosion of values in training
-
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
@@ -38,16 +17,30 @@ class Activation_ReLU:
     def forward(self, inputs):
         self.output = np.maximum(0,inputs)
 
+class Activation_Softmax:
+    def forward(self,inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis = 1, keepdims = True))        
+        probabilities = exp_values/np.sum(exp_values,axis=1, keepdims=True)
+        self.output = probabilities
         
-
-layer1 = Layer_Dense(2,5)
+X,y = spiral_data(samples = 100, classes = 3)
+dense1 = Layer_Dense(2,3)
 activation1 = Activation_ReLU()
 
+dense2 = Layer_Dense(3,3)
+activation2 = Activation_Softmax()
 
-layer1.forward(X)
-# print(layer1.output)
-activation1.forward(layer1.output)
-print(activation1.output)
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
+
+
+
+
 
 
 
